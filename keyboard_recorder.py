7c17541bf735ae1d53df6f6f785972e78029af48
isingmodel <astroblasterr@gmail.com>
2020-04-05 01:01:34 +0900

    Initial skeleton

---
+++ keyboard_recorder.py
@@ -0,0 +1,58 @@
+from pynput import keyboard
+import time
+from multiprocessing import Process
+key_value_list = list()
+key_value_list_press = list()
+keyboard_button = keyboard.Controller()
+keyboard_key = keyboard.Key
+
+
+def on_press(key_data):
+    ts = time.time()
+    key_value = None
+    key_type = None
+    try:
+        key_value = str(key_data.char)
+        key_type = "char"
+    except AttributeError:
+        key_value = str(key_data)
+        key_type = "else"
+    finally:
+        print(key_value)
+        key_value_list.append((key_value, ts))
+        key_value_list_press.append((key_value, key_type, ts))
+
+
+def on_release(key_data):
+    ts = time.time()
+    key_value_raw = str(key_data)
+    if key_value_raw.startswith("'") and key_value_raw.endswith("'"):
+        key_value = key_value_raw.strip("'") + '_'
+    else:
+        key_value = key_value_raw + '_'
+
+    key_value_list.append((key_value, ts))
+
+    if key_data == keyboard.Key.esc:
+        return False
+
+
+def start_keyboard_logging(input_listener):
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    with listener:
+        listener.join()
+
+    return listener, key_value_list_press
+
+
+if __name__ == "__main__":
+    p = Process(target=start_keyboard_logging, args=('bob',))
+    p.start()
+    p.join()
+    time.sleep(0.5)
+    keyboard_button.press("w")
+    keyboard_button.release("w")
+    time.sleep(3)
+    keyboard_button.press(keyboard_key.esc)
+    keyboard_button.release(keyboard_key.esc)
+    # listener, keyboard_log_data = start_keyboard_logging()
---
+++ qt_text_ui.py
@@ -0,0 +1,59 @@
+from sys import exit as sysExit
+
+from PyQt5.QtCore import Qt
+# from PyQt5.QtGui     import
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtWidgets import QPlainTextEdit, QHBoxLayout
+from PyQt5 import QtCore
+
+import time
+
+
+class CenterObject(QPlainTextEdit):
+    def __init__(self):
+        QPlainTextEdit.__init__(self)
+
+        self.setAttribute(QtCore.Qt.WA_InputMethodEnabled, True)
+        self.inputMethodQuery(Qt.ImEnabled)
+
+
+class CenterPane(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.objCntrPane = CenterObject()
+        self.objCntrPane.textChanged.connect(self.save_realtime_text)
+
+        hbox = QHBoxLayout(self)
+        hbox.addWidget(self.objCntrPane)
+        self.objCntrPane.insertPlainText("doing.\n")
+
+    def save_realtime_text(self):
+        print(self.objCntrPane.toPlainText(), time.time(), "\n")
+        # print(self.objCntrPane.cursorRect())
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        winLeft = 100
+        winTop = 100
+        winWidth = 400
+        winHeight = 300
+
+        self.setWindowTitle('Main Window')
+        self.setGeometry(winLeft, winTop, winWidth, winHeight)
+        self.setCentralWidget(CenterPane())
+
+
+
+
+
+# This routine needs to be made as simple as possible
+if __name__ == "__main__":
+    app = QApplication([])
+
+    GUI = MainWindow()
+    GUI.show()
+
+    sysExit(app.exec_())
