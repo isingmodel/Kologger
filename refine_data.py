e28b61e0ef9a86b05b471a395d69e61b2224f985
isingmodel <astroblasterr@gmail.com>
2020-05-20 00:51:20 +0900

    refactoring, key name error fix, key release time matching error

--- refine_data.py
+++ refine_data.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import pickle as pkl
 import numpy as np
 from pprint import pprint
+
 DOUBLE_JUNG_LIST = list('ㅘㅙㅚㅝㅞㅟㅢ')
 DOUBLE_JUNG_DICT = {'ㅘ': ['ㅗ', 'ㅏ'], 'ㅙ': ['ㅗ', 'ㅐ'], 'ㅚ': ['ㅗ', 'ㅣ'], 'ㅝ': ['ㅜ', 'ㅓ'], 'ㅞ': ['ㅜ', 'ㅔ'],
                     'ㅟ': ['ㅜ', 'ㅣ'], 'ㅢ': ['ㅡ', 'ㅣ']}
@@ -20,11 +21,11 @@ kor_to_eng_dict = dict()
 for i in range(len(eng)):
     kor_to_eng_dict[kor[i]] = eng[i]
 
-
 special_key_dict = dict()
 special_key_dict['\x08'] = 'Key.backspace'
 special_key_dict['\r'] = 'Key.enter'
 special_key_dict[' '] = 'Key.space'
+
 
 def split(letter):
     decomposed = hgtk.letter.decompose(letter)
@@ -61,10 +62,10 @@ def refine_ui_data_init(ui_data):
     for i in range(len(ui_data_refined)):
         ui_data_copy[i] = list(ui_data_copy[i])
         if ui_data_refined[i][1] == '\x7f':
-                continue
+            continue
 
         if ui_data_refined[i][2] == "":
-            
+
             # useless
             if ui_data_refined[i][1] == " ":
                 continue
@@ -112,30 +113,31 @@ def list_to_pandas(d_type, d_list):
 
 
 def match_pynput_press_release(key_d):
-    key_data = [list(i) for i in key_d if i[4] =='press']
+    key_data = [list(i) for i in key_d if i[4] == 'press']
     # print(key_d)
-    release_data = [list(i) for i in key_d if i[4] =='release']
+    release_data = [list(i) for i in key_d if i[4] == 'release']
     press_data_len = len(key_data)
     for idx, press_key in enumerate(key_data):
-    #     print(idx, release_data[idx])
-        idx_search = idx-3
+        #     print(idx, release_data[idx])
+        idx_search = idx - 3
         while True:
-            # print(press_key[1], release_data[idx_search][1])
-            if press_data_len == idx_search:
-                print("find release key error")
-                break
-            elif press_key[1] == release_data[idx_search][1] and release_data[idx_search][4] == "release":
-                # print(key_data[idx][3] - release_data[idx_search][3])
+            #             print(press_key[1], release_data[idx_search][1], release_data[idx_search][4])
+            if press_key[1] == release_data[idx_search][1] and release_data[idx_search][4] == "release":
                 key_data[idx][4] = release_data[idx_search][3]
                 release_data[idx_search][4] = "done"
                 break
+            elif press_data_len == idx_search:
+                print(press_data_len, idx_search, press_key, idx)
+                print("find release key error")
+                break
+
             else:
                 idx_search += 1
     # print(key_data)
-    return key_data                
+    return key_data
+
 
 def refine_all_data(ui_d, key_d):
-
     ui_d_refined = refine_ui_data_init(ui_d)
     # print(ui_d_refined)
     key_d_refined = match_pynput_press_release(refine_data_kbd_init(key_d))
@@ -151,11 +153,10 @@ def refine_all_data(ui_d, key_d):
         click += [0., 0.]
     Korkey = [click[1] for click in ui_d_refined_2]
 
-
     for kor_idx in range(len(Korkey)):
         if Korkey[kor_idx] == "\x08":
             target_key = special_key_dict[Korkey[kor_idx]]
-        elif Korkey[kor_idx] in eng: 
+        elif Korkey[kor_idx] in eng:
             target_key = Korkey[kor_idx]
         elif Korkey[kor_idx] in kor:
             target_key = kor_to_eng_dict[Korkey[kor_idx]]
@@ -163,16 +164,16 @@ def refine_all_data(ui_d, key_d):
             target_key = Korkey[kor_idx]
         else:
             target_key = special_key_dict[Korkey[kor_idx]]
-        
+
         candidate = np.searchsorted(ts_eng_press, ts[kor_idx], side='right')
-        
-        for key_idx in range(max(candidate-5, 0), candidate):
+
+        for key_idx in range(max(candidate - 5, 0), candidate):
             if target_key == Engkey[key_idx] and key_d_refined_2[key_idx][5] == 0:
                 ui_d_refined_2[kor_idx][4] = ts_eng_press[key_idx]
                 ui_d_refined_2[kor_idx][6] = ts_eng_release[key_idx]
                 key_d_refined_2[key_idx][5] = 1
 
-                ui_d_refined_2[kor_idx][7] = 1 # need it?
+                ui_d_refined_2[kor_idx][7] = 1  # need it?
 
                 break
 
@@ -184,27 +185,27 @@ def refine_all_data(ui_d, key_d):
     ui_d_refined_3 = list()
     for rank in ranks:
         ui_d_refined_3.append(ui_d_refined_2[rank][:7])
-    
+
     for i in range(len(ui_d_refined_3)):
         if ui_d_refined_3[i][1] == '\r':
-            ui_d_refined_3[i][1] = 'Key.Enter'
+            ui_d_refined_3[i][1] = 'Key.enter'
         if ui_d_refined_3[i][1] == '\x08':
             ui_d_refined_3[i][1] = 'Key.backspace'
     for pynput_key in key_d_refined_2:
         if pynput_key[1].startswith("Key"):
-            if pynput_key[1] not in ['Key.Enter', 'Key.backspace']:
+            if pynput_key[1] not in ['Key.enter', 'Key.backspace']:
                 ui3_ts = np.array([u[4] for u in ui_d_refined_3])
                 key_idx = np.searchsorted(ui3_ts, pynput_key[3])
                 # todo: need to support key_idx = 0
                 if key_idx != 0:
-                    ui_before = ui_d_refined_3[key_idx-1]
+                    ui_before = ui_d_refined_3[key_idx - 1]
                     new_input = [1, pynput_key[1], "", ui_before[3], pynput_key[3], ui_before[5], pynput_key[4]]
                 else:
                     new_input = [1, pynput_key[1], "", "", pynput_key[3], 0, pynput_key[4]]
                 ui_d_refined_3.insert(key_idx, new_input)
 
     return ui_d_refined_3
-   
+
 
 if __name__ == "__main__":
     ui_p = "./temp_data/ui_data.pkl"
@@ -220,5 +221,3 @@ if __name__ == "__main__":
     # pprint(refined_data)
     ui_df = list_to_pandas('ui', refined_data)
     ui_df.to_csv(Path("./ui_data_sj.csv"))
-
-
